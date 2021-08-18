4a570a12dfd025efb757b5f6db0affda1258a29e
Thant Ko Zaw <thantmkou10@gmail.com>
2021-08-18 23:37:59 +0630

    Add more samples

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,6 +1,6 @@
 def bubble_sort(array)
   sorted = true
-  
+
   array.size.times do
     for i in 0..array.size-2
       if array[i] > array[i+1]
@@ -18,3 +18,5 @@ end
 
 bubble_sort([6,5,4,3,2,1]) # [1, 2, 3, 4, 5, 6]
 bubble_sort([4,3,78,2,0,2]) # [0, 2, 2, 3, 4, 78]
+bubble_sort(['a', 'c', 'b', 'z', 'h']) # ["a", "b", "c", "h", "z"]
+bubble_sort(['Michael', 'McGregor', 'Marco', 'Main']) # ["Main", "Marco", "McGregor", "Michael"]
