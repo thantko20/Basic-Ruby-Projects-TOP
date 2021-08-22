9110c2efb6e0b03120cc14304dfede4a4f15647b
Thant Ko Zaw <thantmkou10@gmail.com>
2021-08-22 19:01:47 +0630

    Auto-correct caesar.rb using rubocop

--- caesar.rb
+++ caesar.rb
@@ -1,19 +1,19 @@
 def caesar_cipher
-  print "Text: "
+  print 'Text: '
   string = gets.to_s
   key = 0
   loop do
-    print "Key: "
+    print 'Key: '
     key = gets.to_i
-    key > 0 ? break : (p "Key must be positive integer!")
+    key > 0 ? break : (p 'Key must be positive integer!')
   end
 
   new_array = string.chars.map { |char| char.ord }
   new_array.map! do |num|
-    if num.between?(65, 90) 
+    if num.between?(65, 90)
       num = ((num - 65 + key) % 26) + 65
       num = num.chr
-    elsif num.between?(97, 122) 
+    elsif num.between?(97, 122)
       num = ((num - 97 + key) % 26) + 97
       num = num.chr
     end
