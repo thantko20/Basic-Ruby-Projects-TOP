42d6d6c6d223a7c5204343744e2083ae5920c38b
Thant Ko Zaw <thantmkou10@gmail.com>
2021-08-23 09:20:07 +0630

    Remove comments

--- substrings.rb
+++ substrings.rb
@@ -2,15 +2,12 @@ dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","o
 
 def substrings(string, dict)
   hash = Hash.new
-  # Splitting the input string and coverting the letters into lowercase
   checker = string.split.map! { |item| item.downcase}
-  # Loop through each word of dictionary
+
   dict.each do |word|
-    # Loop through each word of checker
     checker.each do |item|
-      # If checker has the same word in dictionary,
       if item.include?(word)
-        hash[word] = 0 unless hash[word] # Create a new key-value pair if there's not such key
+        hash[word] = 0 unless hash[word]
         hash[word] += 1
       end
     end
